fa633ab6ec5e0f415646b366333f2442f49ab8b1
aldomq <58537416+aldomq@users.noreply.github.com>
2025-12-02 21:16:26 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/proyecto_datos/Proyecto_datos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_datos;
+
+/**
+ *
+ * @author aldoi
+ */
+public class Proyecto_datos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
